4e15ef18831ce59761bce382475f47c04b4bb2d6
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-09-14 00:49:36 +0600

    отображаем содержимое БД на домашней странице (соединяем представления, шаблоны и URL)

--- Course_SecondProject/settings.py
+++ Course_SecondProject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'onetoone.apps.OnetooneConfig',
     'onetomany.apps.OnetomanyConfig',
     'manytomany.apps.ManytomanyConfig',
+    'orm_method.apps.OrmMethodConfig',
     
 
 ]
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'Course_SecondProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
